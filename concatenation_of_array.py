5190466a21edc00965c71c3b52b370068094ff62
Ash <hrksingh17@gmail.com>
2025-09-09 11:06:42 +0530

    Concatenation of Array

---
+++ concatenation_of_array.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        ans = []
+        for i in range(2):
+            for num in nums:
+                ans.append(num)
+        return ans
+        
+print(Solution().getConcatenation([1,2,3]))
