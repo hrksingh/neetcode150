5adc7fd0e438f81dbecb59b12c3152f8a448d110
Ash <hrksingh17@gmail.com>
2025-11-20 11:27:37 +0530

    two integer sum II

---
+++ two_integer_sum_II.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l, r = 0, len(numbers) - 1
+
+        while l < r:
+            curSum = numbers[l] + numbers[r]
+            if curSum > target:
+                r -= 1
+            elif curSum < target:
+                l += 1
+            else:
+                return [l + 1, r + 1]
+        return []
+
+
+numbers = [1, 2, 3, 4]
+print(Solution().twoSum(numbers, 3))
