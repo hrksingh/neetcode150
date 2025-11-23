cba1a56110330bd3cdd42bcccac543bedb1120ae
Ash <hrksingh17@gmail.com>
2025-11-23 21:36:42 +0530

    boats to save people

---
+++ boats_to_save_people.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        boat = 0
+        people.sort()
+        l, r = 0, len(people) - 1
+
+        while l <= r:
+            if people[l] + people[r] <= limit:
+                l += 1
+            r -= 1
+            boat += 1
+
+        return boat
+
+
+people = [3, 2, 2, 1]
+print(Solution().numRescueBoats(people, 3))
