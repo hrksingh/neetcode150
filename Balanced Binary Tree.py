3e160be1ce7186a1756a901786c9f598065041d1
Ash <hrksingh17@gmail.com>
2026-01-12 17:51:02 +0530

    Balanced Binary Tree

---
+++ Balanced Binary Tree.py
@@ -0,0 +1,31 @@
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        balanced = [True]
+
+        def height(node):
+            if not root:
+                return 0
+
+            left = height(node.left)
+            if balanced[0] is False:
+                return 0
+            right = height(node.right)
+
+            if abs(left - right) > 1:
+                balanced[0] = False
+                return 0
+
+            return 1 + max(left, right)
+
+        return balanced[0]
