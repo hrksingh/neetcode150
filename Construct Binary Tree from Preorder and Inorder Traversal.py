491482238907a919b7f0782c7e3ab0efbe21d4d2
Ash <hrksingh17@gmail.com>
2026-01-30 00:13:29 +0530

    -

--- Construct Binary Tree from Preorder and Inorder Traversal.py
+++ Construct Binary Tree from Preorder and Inorder Traversal.py
@@ -22,6 +22,25 @@ class Solution:
         root.right = self.buildTree(preorder[mid + 1 :], inorder[mid + 1 :])
         return root
 
+    def buildTreeUsingMap(
+        self, preorder: List[int], inorder: List[int]
+    ) -> Optional[TreeNode]:
+        inorder_map = {val: idx for idx, val in enumerate(inorder)}
+        self.preorder_idx = 0
+
+        def build(l, r):
+            if l > r:
+                return None
+            val = preorder[self.preorder_idx]
+            self.preorder_idx += 1
+            root = TreeNode(val)
+            mid_idx = inorder_map[val]
+            root.left = build(l, mid_idx - 1)
+            root.right = build(mid_idx + 1, r)
+            return root
+
+        return build(0, len(inorder) - 1)
+
 
 def print_tree(
     node: Optional[TreeNode], prefix: str = "", is_left: bool = True
@@ -39,4 +58,5 @@ if __name__ == "__main__":
     preorder = [40, 20, 10, 30, 60, 50, 70]
     inorder = [10, 20, 30, 40, 50, 60, 70]
     root = Solution().buildTree(preorder, inorder)
+    root2 = Solution().buildTreeUsingMap(preorder, inorder)
     print_tree(root)
