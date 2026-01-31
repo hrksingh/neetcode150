80f43272181f8b712472179b1a5c1f59ed1fadb5
Ash <hrksingh17@gmail.com>
2026-01-31 21:29:13 +0530

    -

--- House Robber III.py
+++ House Robber III.py
@@ -11,4 +11,15 @@ class TreeNode:
 
 class Solution:
     def rob(self, root: Optional[TreeNode]) -> int:
-        return 0
+        def dfs(root):
+            if not root:
+                return [0, 0]  # [root, withoutRoot]
+
+            leftPair = dfs(root.left)
+            rightPair = dfs(root.right)
+
+            withRoot = root.val + leftPair[1] + rightPair[1]
+            withOutRoot = max(leftPair) + max(rightPair)
+            return [withRoot, withOutRoot]
+
+        return max(dfs(root))
