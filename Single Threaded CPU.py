f9359e7cb8c109733db6a918f8699e57ef4bf67c
Ash <hrksingh17@gmail.com>
2026-02-26 05:39:45 +0530

    Single Threaded CPU

--- Single Threaded CPU.py
+++ Single Threaded CPU.py
@@ -1,13 +1,27 @@
+import heapq
 from typing import List
 
 
 class Solution:
     def getOrder(self, tasks: List[List[int]]) -> List[int]:
-        for i, t in enumerate(tasks):
+        for i, _ in enumerate(tasks):
             tasks[i].append(i)
         tasks.sort(key=lambda t: t[0])
-        print(tasks)
-        res = []
+
+        heap, res = [], []
+        time, i = 0, 0
+        while heap or i < len(tasks):
+            if not heap:
+                time = tasks[i][0]
+
+            while i < len(tasks) and time >= tasks[i][0]:
+                heapq.heappush(heap, (tasks[i][1], tasks[i][2]))
+                i += 1
+
+            if heap:
+                proc_time, curr_idx = heapq.heappop(heap)
+                time += proc_time
+                res.append(curr_idx)
         return res
 
 
