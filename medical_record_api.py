58b666dd56e89136b3f0a33e21c9f454e94dc41a
Ash <hrksingh17@gmail.com>
2026-03-01 00:13:11 +0530

    httpx

--- medical_record_api.py
+++ medical_record_api.py
@@ -1,3 +1,4 @@
+import httpx
 import requests
 
 BASE_URL = "https://jsonmock.hackerrank.com/api/medical_records"
@@ -40,3 +41,33 @@ def getData(doctorName, diagnosisId):
 
 
 print(getData("Dr Arnold Bullock", 2))
+
+
+def getDataHttpx(doctorName, diagnosisId):
+    page = 1
+    min_temp = float("inf")
+    max_temp = float("-inf")
+    while True:
+        resp = httpx.get(BASE_URL, params={"page": page}).raise_for_status().json()
+        data = resp.get("data", [])
+        for record in data:
+            if (
+                record.get("doctor", {}).get("name") == doctorName
+                and record.get("diagnosis", {}).get("id") == diagnosisId
+            ):
+                temp = record.get("vitals", {}).get("bodyTemperature")
+                if temp is not None:
+                    min_temp = min(min_temp, temp)
+                    max_temp = max(max_temp, temp)
+
+        if page >= resp.get("total_pages", 0):
+            break
+
+        page += 1
+    if min_temp == float("inf"):
+        return []
+
+    return [int(min_temp), int(max_temp)]
+
+
+print(getDataHttpx("Dr Arnold Bullock", 2))
